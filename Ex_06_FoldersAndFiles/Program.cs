1114b6e40f78c7653755489fdab92ce006bd5bcc
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-19 19:48:16 +0300

    add Ex_06

---
+++ Ex_06_FoldersAndFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿// Как посмотреть содержимое папки?
+
+void CatalogInfo(string path, string indent = "")
+{
+DirectoryInfo catalogs = new DirectoryInfo(path);
+foreach (var currentCatalog in catalogs.GetDirectories())
+{
+Console.WriteLine($"{indent}{currentCatalog.Name}");
+CatalogInfo(currentCatalog.FullName, indent + " ");
+}
+foreach (var item in catalogs.GetFiles())
+{
+Console.WriteLine($"{indent}{item.Name}");
+}
+}
+string path = @"D:\XOPEK\8_Прочее\Java_II\1_Незавершенка\Знакомство с языком C#\Урок 7. 19.02.2023 Рекурсия продолжение\Less7";
+CatalogInfo(path);
